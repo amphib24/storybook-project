6fe86eb2b0824a1cac0d24edb47e6903dca5cfc1
Siamak Broomand <broomand.siamk85@gmail.com>
2016-12-19 15:16:37 -0800

    Siamak changes constructor

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var storyImage = document.getElementById('storyImage');
 
 //Global variables
 var allImages = [];
-var imgName = ('beanstalk', 'book', 'gorilla', 'humpty', 'moon', 'oz', 'tiger');
+var imgName = ['beanstalk', 'book', 'gorilla', 'humpty', 'moon', 'oz', 'tiger'];
 
 
 
@@ -22,3 +22,13 @@ var imgName = ('beanstalk', 'book', 'gorilla', 'humpty', 'moon', 'oz', 'tiger');
 
 
 //Constructor
+//to get images from the array, to use later for local storage
+for (var i = 0; i < imgName.length; i++){
+  new ImgGenerator(imgName[i]);
+}
+
+function ImgGenerator(name){
+  this.names = name;
+  this.filePath = 'img/' + name + '.jpg';
+  allImages.push(this);
+}
