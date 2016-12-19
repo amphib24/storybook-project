8183491642e1b37657541a46449d4f9c32bf164b
Annie Ton-Nu <annieqton@gmail.com>
2016-12-19 14:54:45 -0800

    annie - created DOM and Global variables

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//DOM variables
+var storyImage = document.getElementById('storyImage');
+
+
+
+
+
+
+
+
+
+//Global variables
+var allImages = [];
+var imgName = ('beanstalk', 'book', 'gorilla', 'humpty', 'moon', 'oz', 'tiger');
+
+
+
+
+
+
+
+//Constructor
