31d523b6a7e1db96729d7550a1cd30f57b07111b
JEFF BLANCHARD <BLANCHARDJD19@COMCAST.NET>
2016-12-19 16:37:44 -0800

    pushing changes to js so far - jeff

--- app.js
+++ app.js
@@ -19,16 +19,39 @@ var imgName = ['beanstalk', 'book', 'gorilla', 'humpty', 'moon', 'oz', 'tiger'];
 
 
 
-
-
-//Constructor
 //to get images from the array, to use later for local storage
 for (var i = 0; i < imgName.length; i++){
   new ImgGenerator(imgName[i]);
 }
 
+//Constructor
 function ImgGenerator(name){
   this.names = name;
   this.filePath = 'img/' + name + '.jpg';
+  this.used = false;
   allImages.push(this);
 }
+
+
+ //////// create random image generator
+ function randImg () {
+   var img = return Math.floor(Math.random() * allImages.length);
+   storyImage.appendChild(img[i]);
+ }
+
+function displayImg (){
+
+  var x = randImg();
+
+while (allImages[x].used !== true) {
+
+}
+
+
+
+    storyImage.src = allImages[newImage[i]].filePath;
+
+     randImg();
+  }
+createImg();
+/////display image
