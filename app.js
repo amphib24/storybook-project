c645ff579f03e0faa23a59de80ce37ac6db97746
Annie Ton-Nu <annieqton@gmail.com>
2016-12-22 19:10:30 -0800

    user sign in, image/title/story input save feature

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ var storyImage = document.getElementById('storyImage');
 var changeImageButton = document.getElementById('changeImageButton');
 var textBox = document.getElementById('textBox');
 var clearText = document.getElementById('clearText');
-
+var storyForm = document.getElementById('storyForm');
+var storyTitle = document.getElementById('storyTitle');
 
 
 
 //Global variables
 var allImages = [];
-var imgName = ['beanstalk', 'gorilla', 'humpty', 'moon', 'oz', 'tiger', 'batman', 'boyanddog', 'boydogplay', 'cars', 'cow', 'dragoncarrot', 'dreams', 'jellyfish', 'junglebook', 'lightingmcqueen', 'mice', 'monster', 'moon','ori-and-the-blind-forest', 'oz', 'pirate', 'roof', 'safari', 'sheeps', 'transformer'];
+var imgName = ['beanstalk', 'gorilla', 'humpty', 'moon', 'oz','tiger', 'batman', 'boyanddog', 'boydogplay', 'cars', 'cow', 'dragoncarrot', 'dreams', 'jellyfish', 'junglebook', 'lightingmcqueen', 'mice', 'monster', 'moon','ori-and-the-blind-forest', 'pirate', 'roof', 'safari', 'sheeps', 'transformer'];
 
 
 
@@ -26,14 +27,13 @@ var imgName = ['beanstalk', 'gorilla', 'humpty', 'moon', 'oz', 'tiger', 'batman'
 
 
 //to get images from the array, to use later for local storage
-for (var i = 0; i < imgName.length; i++){
-  new ImgGenerator (imgName[i]);
-}
 
 //Constructor
 function ImgGenerator(name){
-  this.names = name;
+  this.name = name;
   this.filePath = 'img/' + name + '.jpg';
+  this.title = '';
+  this.story = '';
   this.used = false; // meaning image hasn't been used before
   allImages.push(this);
 }
@@ -44,12 +44,14 @@ function randImg() {
   var randomNumber = Math.floor(Math.random() * allImages.length);
   var theImage = allImages[randomNumber];
   return theImage;
+
 }
 
 var counter = 0;
 
 function displayImg() {
-
+  storyTitle.value = null;
+  textBox.value = null;
   var imageObject = randImg();
 
   while (imageObject.used === true) {  // meaning images hasn't been used before
@@ -59,12 +61,15 @@ function displayImg() {
   imageObject.used = true;
 
   if (storyImage){
-  storyImage.src = imageObject.filePath;
+    storyImage.src = imageObject.filePath;
+    storyImage.alt = imageObject.name;
+    storyTitle.value = imageObject.title;
+    textBox.value = imageObject.story;
   }
   // reset check when array runs out .
   counter += 1;
 
-  if (counter % 26 === 0) {
+  if (counter % allImages.length === 0) {
 
     allImages.forEach(function (element) {
       element.used = false;
@@ -75,7 +80,35 @@ function displayImg() {
   // console.table(allImages);
 }
 
+function handleStoryForm(event) {
+  event.preventDefault();
+
+  for(var i = 0; i < allImages.length; i++) {
+    if(storyImage.alt === allImages[i].name) {
+      allImages[i].title = event.target.formTitle.value;
+      allImages[i].story = event.target.formStory.value;
+      localStorage.setItem('allImages', JSON.stringify(allImages));
+    }
+  }
+  // event.target.formTitle.value = null;
+  // event.target.formStory.value = null;
+}
+
+function checkImages() {
+  if(localStorage.allImages){
+    var retrieveStorage = localStorage.getItem('allImages');
+    allImages = JSON.parse(retrieveStorage);
+    for (var i = 0; i < allImages.length; i++) {
+      allImages[i].used = false;
+    }
+  } else {
+    for (var i = 0; i < imgName.length; i++){
+      new ImgGenerator (imgName[i]);
+    }
+  }
+}
 // function execution
+checkImages();
 
 displayImg();
 function changeImageHandler(event){
@@ -85,10 +118,10 @@ function changeImageHandler(event){
 
 function clearTextHandler(event){
   event.preventDefault();
-  console.log('event listener working');
   textBox.value = '';
-  console.log(textBox);
+  storyTitle.value = '';
 }
 
 clearText.addEventListener('click', clearTextHandler);
 changeImageButton.addEventListener('click', changeImageHandler);
+storyForm.addEventListener('submit', handleStoryForm);
